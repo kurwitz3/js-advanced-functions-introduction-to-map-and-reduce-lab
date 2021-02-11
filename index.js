7b7c90fdd9f4c91d033cd30370e409b4af9e4ee2
kurwitz3 <kurwitz3@aol.com>
2021-02-11 12:51:35 -0500

    commit tree 97bf660bdd928a03bade8d5d55e2a78989365789

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+function mapToNegativize(array){
+  return array.map(x => x * -1)
+}
+
+function mapToNoChange(array){
+  return array.map(x => x)
+}
+
+function mapToDouble(array){
+  return array.map(x => x * 2)
+}
+
+function mapToSquare(array){
+    return array.map(x => x * x)
+}
+
+function reduceToTotal(array,initialValue = 0){
+ let sum =   array.reduce((acc,value,array,initialValue) => acc + value,initialValue)
+  return sum
+}
+
+function reduceToAllTrue(array){
+  const reducer = function(acc, value){  
+    if(!!acc == true && !!value == true){
+        return true;
+    } else {
+        return false;
+      }
+  }
+    return array.reduce(reducer, true)
+}
+function reduceToAnyTrue(array){
+    const reducer = function(acc, value){  
+        if(acc == true){
+            return true
+        } else {
+            return !!value
+        } 
+    }
+    return array.reduce(reducer, false)
+
+}
